c781bd5f58593076ac79b7fc6edbfb9bda1c1a5d
reeps3 <mpharm3@gmail.com>
2019-05-27 14:02:41 +0300

    added sending data from contact form and changing logo while scrolling page

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
 from flask_bootstrap import Bootstrap
 from flask_babel import Babel
+from flask_mail import Mail
+from config import Config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-app.config['LANGUAGES'] = ['en', 'uk', 'pseudo', 'ids']
+app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
 babel = Babel(app)
+mail = Mail(app)
 
 
 @babel.localeselector
--- config.py
+++ config.py
@@ -7,18 +7,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT =  int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['oksana@metamova.com']
-    LANGUAGES = ['en', 'es']
+    ADMINS = os.environ.get('ADMINS')
+    LANGUAGES = os.environ.get('LANGUAGES')
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    # ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
     POSTS_PER_PAGE = 25
+    
